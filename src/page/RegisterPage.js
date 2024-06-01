35f95df81347516afefa5b23de15381a0a9df1f2
yangshoong <crinaeae83@gmail.com>
2024-06-02 05:52:27 +0900

    로그인 실행 수정

--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
